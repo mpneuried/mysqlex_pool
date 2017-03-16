1abeddfab0341f3fc3a63fe74dc228af214c2ef6
M. Peter <mpneuried@googlemail.com>
2017-03-16 17:13:33 +0100

    updated configs and readme

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # MysqlexPool
 
-**TODO: Add description**
+[![Travis Build Status](https://img.shields.io/travis/mpneuried/mysqlex_pool.svg)](https://travis-ci.org/mpneuried/mysqlex_pool)
+[![Windows Tests](https://img.shields.io/appveyor/ci/mpneuried/mysqlex_pool.svg?label=WindowsTest)](https://ci.appveyor.com/project/mpneuried/mysqlex_pool)
+[![Coveralls Coverage](https://img.shields.io/coveralls/mpneuried/mysqlex_pool.svg)](https://coveralls.io/github/mpneuried/mysqlex_pool)
+
+[![Hex.pm Version](https://img.shields.io/hexpm/v/mysqlex_pool.svg)](https://hex.pm/packages/mysqlex_pool)
+[![Deps Status](https://beta.hexfaktor.org/badge/all/github/mpneuried/mysqlex_pool.svg?branch=master)](https://beta.hexfaktor.org/github/mpneuried/mysqlex_pool)
+[![Hex.pm](https://img.shields.io/hexpm/dt/mysqlex_pool.svg?maxAge=2592000)](https://hex.pm/packages/mysqlex_pool)
+
+
+A warpper for mysqlex to add connection pooling with poolboy
 
 ## Installation
 
@@ -12,6 +21,11 @@ def deps do
   [{:mysqlex_pool, "~> 0.1.0"}]
 end
 ```
+
+## TODOs
+
+- connection config ;-)
+- docs
 
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
--- mix.exs
+++ mix.exs
@@ -1,38 +1,64 @@
 defmodule MysqlexPool.Mixfile do
-  use Mix.Project
+	use Mix.Project
 
-  def project do
-    [app: :mysqlex_pool,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
+	defp package do
+		[
+			files: ["lib", "mix.exs", "README.md", "LICENSE"],
+			maintainers: ["M. Peter"],
+			licenses: ["MIT"],
+			links: %{"GitHub" => "https://github.com/mpneuried/mysqlex_pool"}
+		]
+	end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
-     mod: {MysqlexPool, []}]
-  end
+	defp description do
+		"""
+		A warpper for mysqlex to add connection pooling with poolboy
+		"""
+	end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [
-      { :poolboy, "~> 1.5" },
-      { :mysqlex, "~> 0.0.1" },
-      { :benchfella, "~> 0.3.4", only: [ :dev, :test ] }
-    ]
-  end
+	def project do
+		[
+			app: :mysqlex_pool,
+			version: "0.1.0",
+			elixir: "~> 1.4",
+			build_embedded: Mix.env == :prod,
+			start_permanent: Mix.env == :prod,
+			deps: deps(),
+			package: package(),
+			description: description(),
+			docs: [ extras: ["README.md"], main: "readme"],
+			test_coverage: [tool: ExCoveralls]
+		]
+	end
+
+	# Configuration for the OTP application
+	#
+	# Type "mix help compile.app" for more information
+	def application do
+		# Specify extra applications you'll use from Erlang/Elixir
+		[extra_applications: [:logger],
+		 mod: {MysqlexPool, []}]
+	end
+
+	# Dependencies can be Hex packages:
+	#
+	#   {:my_dep, "~> 0.3.0"}
+	#
+	# Or git/path repositories:
+	#
+	#   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+	#
+	# Type "mix help deps" for more examples and options
+	defp deps do
+		[
+			{ :poolboy, "~> 1.5" },
+			{ :mysqlex, "~> 0.0.1" },
+			{ :benchfella, "~> 0.3.4", only: [ :dev, :test ] },
+			{ :dialyze, "~> 0.2", only: :dev},
+			{ :earmark, ">= 0.0.0", only: [:docs, :dev]},
+			{ :ex_doc, ">= 0.0.0", only: [:docs, :dev]},
+			{ :credo, "~> 0.5", only: [:dev, :test]},
+			{ :excoveralls, "~> 0.5", only: [:dev, :test]}
+		]
+	end
 end
